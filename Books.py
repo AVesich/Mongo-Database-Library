efbeeb5b114f6ace079fbab794130b0a1265c966
AVesich <75646434+AVesich@users.noreply.github.com>
2024-10-19 22:13:24 -0400

    Basic add, edit, and remove works

--- Books.py
+++ Books.py
@@ -1,5 +1,136 @@
+import pymongo
+import Shared
+from Shared import client
+
 class BookEngine:
+    db = client['library'] # Library db
+
+    def add_book(self, args: list[str]): # name, authors, isbn, # of pages
+        arg_count = len(args)
+        if arg_count < 4:
+            print("Not enough args provided. Minimum 4 args.")
+            return
+            
+        self.db.books.insert_one({
+            'name' : args[0],
+            'authors' : args[1:arg_count-2],
+            'ISBN': args[arg_count-2],
+            'num_pages': args[arg_count-1]
+        })
+        print("Book added!")
+
+    def remove_book(self, args: list[str]): # isbn
+        arg_count = len(args)
+        if arg_count != 1:
+            print("Incorrect # of args. 1 must be provided.")
+            return
+
+        result = self.db.books.delete_one({'ISBN':args[0]})
+        print(result.deleted_count)
+        print("Book removed!" if (result.deleted_count > 0) else "No book found!")
+
+    def edit_book(self, args: list[str]): # isbn, new name, new authors, new # of pages
+        arg_count = len(args)
+        if arg_count < 4:
+            print("Not enough args provided. Minimum 4 args.")
+            return
+
+        result = self.db.books.update_one({ 'ISBN':args[0] },
+                                          { '$set': {'name':args[1],
+                                                     'authors':args[2:arg_count-1] ,
+                                                     'num_pages': args[arg_count-1]}
+                                          })
+
+        print("Book updated!" if (result.modified_count > 0) else "No books found to update!")
+
+    def search_books(self, args: list[str]): # search type, query
+        arg_count = len(args)
+        if arg_count != 2:
+            print("Incorrect # of args. 2 must be provided.")
+            return
+
+        if args[0].lower() == "name":
+            results = self.db.books.find({ 'name' : args[1] })
+        elif args[0].lower() == "author":
+            results = self.db.books.find({ 'authors' : args[1] })
+        elif args[0].lower() == "isbn":
+            results = self.db.books.find({ 'ISBN' : args[1] })
+        else:
+            print("Please enter a valid search type: 'name', 'author', 'isbn'.")
+            return
+        
+        for result in results:
+            response = "Name: " + result['name'] + ",   Authors: "
+            for author in result['authors'] :
+                response += author + ", "
+            if len(result['authors']) > 0:
+                response = response [0:len(response)-2]
+            response += ",   ISBN: " + result['ISBN']
+            response += ",   # of pages: " + result['num_pages']
+            print(response)
+
+    def list_books(self, args: list[str]): # sort type
+        arg_count = len(args)
+        if arg_count != 1:
+            print("Incorrect # of args. 1 must be provided.")
+            return
+        
+        if args[0].lower() == "name":
+            results = self.db.books.find().sort('name', pymongo.ASCENDING)
+        elif args[0].lower() == "author":
+            results = self.db.books.find().sort('authors', pymongo.ASCENDING)
+        elif args[0].lower() == "isbn":
+            results = self.db.books.find().sort('ISBN', pymongo.ASCENDING)
+        elif args[0].lower() == "page count":
+            results = self.db.books.find().sort('num_pages', pymongo.ASCENDING)
+        else:
+            print("Please enter a valid search type: 'name', 'author', 'isbn', 'page count'.")
+            return
+        
+        for result in results:
+            response = "Name: " + result['name'] + ",   Authors: "
+            for author in result['authors'] :
+                response += author + ", "
+            if len(result['authors']) > 0:
+                response = response [0:len(response)-2]
+            response += ",   ISBN: " + result['ISBN']
+            response += ",   # of pages: " + result['num_pages']
+            print(response)
+
+    def checkout_book(self, args: list[str]): # isbn, username
+        arg_count = len(args)
+        if arg_count != 2:
+            print("Incorrect # of args. 2 must be provided.")
+            return
+        
+        print("Book checked out to borrower!")
+
+    def return_book(self, args: list[str]): # isbn
+        arg_count = len(args)
+        if arg_count != 1:
+            print("Incorrect # of args. 1 must be provided.")
+            return
+        
+        print("Book returned to the library!")
+
+    def check_borrower_of_book(self, args: list[str]): # isbn
+        arg_count = len(args)
+        if arg_count != 1:
+            print("Incorrect # of args. 1 must be provided.")
+            return
+        
+        print("Book not borrowed!")
+
+    possible_commands = {
+        "add book" : add_book,
+        "rm book" : remove_book,
+        "edit book" : edit_book,
+        "search books" : search_books,
+        "list books" : list_books,
+        "checkout book" : checkout_book,
+        "return book" : return_book,
+        "borrower of" : check_borrower_of_book
+    }
+
     def handle_input(self, command: str, args: list[str]):
-        print("Command: " + command + "\n")
-        for arg in args:
-            print("Args: " + arg + "\n")
+        self.possible_commands[command](self, args)
--- Borrowers.py
+++ Borrowers.py
@@ -1,5 +1,79 @@
+import pymongo
+import Shared
+from Shared import client
+
 class BorrowerEngine:
+
+    db = client['library'] # Library db
+
+    def add_borrower(self, args: list[str]): # name, username, phone
+        arg_count = len(args)
+        if arg_count != 3:
+            print("Incorrect # of args. 3 must be provided.")
+            return
+        
+        self.db.borrowers.insert_one({
+            'name': args[0],
+            'username': args[1],
+            'phone': args[2]
+        })
+        print("Borrower added!")
+
+    def remove_borrower(self, args: list[str]): # username
+        arg_count = len(args)
+        if arg_count != 1:
+            print("Incorrect # of args. 1 must be provided.")
+            return
+        
+        result = self.db.borrowers.delete_one({'username': args[0]})
+
+        print("Borrower removed!" if (result.deleted_count > 0) else "No borrower found!")
+
+    def edit_borrower(self, args: list[str]): # username, new name, new phone
+        arg_count = len(args)
+        if arg_count != 3:
+            print("Incorrect # of args. 3 must be provided.")
+            return
+        
+        self.db.borrowers.update_one({'username': args[0]},
+                                     { '$set': {'name': args[1],
+                                                'phone': args[2]}
+                                     })
+
+        print("Borrower updated!")
+
+    def check_books_borrowed_by(self, args: list[str]): # username
+        arg_count = len(args)
+        if arg_count != 1:
+            print("Incorrect # of args. 1 must be provided.")
+            return
+        
+        print("No books found!")
+
+    def search_borrowers(self, args: list[str]): # search type, query
+        arg_count = len(args)
+        if arg_count != 2:
+            print("Incorrect # of args. 2 must be provided.")
+            return
+
+        if args[0].lower() == "name":
+            results = self.db.borrowers.find({ 'name' : args[1] })
+        elif args[0].lower() == "username":
+            results = self.db.borrowers.find({ 'username' : args[1] })
+        else:
+            print("Please enter a valid search type: 'name', 'author', 'isbn'.")
+            return
+        
+        for result in results:
+            print("Name: " + result['name'] + ",   Username: " + result['username'] + ",   Phone number: " + result['phone'])
+
+    possible_commands = {
+        "add borrower" : add_borrower,
+        "rm borrower" : remove_borrower,
+        "edit borrower" : edit_borrower,
+        "borrowed by" : check_books_borrowed_by,
+        "search borrowers" : search_borrowers
+    }
+
     def handle_input(self, command: str, args: list[str]):
-        print("Command: " + command)
-        for arg in args:
-            print("Args: " + arg + "\n")
+        self.possible_commands[command](self, args)
